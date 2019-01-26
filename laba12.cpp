7112348894961bea7a9fbad28cabc7a86787ff39
capelson <33690734+capelson@users.noreply.github.com>
2019-01-26 17:51:20 +0200

    Add files via upload

---
+++ laba12.cpp
@@ -0,0 +1,33 @@
+
+#include "stdafx.h"
+#include <iostream>
+#include <conio.h>
+#include <ctype.h>
+#include <string.h>
+using namespace std;
+char make_big(char *str)
+{
+	char alpha[] = "qwertyuiopasdfghjklzxcvbnm";
+
+	for (int i = 0; i < strlen(str); i++)
+	{
+
+		for (int j = 0; j < strlen(alpha); j++)
+		{
+			if (str[i] == alpha[j])
+				str[i] = toupper(str[i]);
+
+		}
+	}
+	cout << str;
+	return 0;
+}
+void main()
+{
+	char line[500];
+	gets_s(line);
+	make_big(line);
+	
+    _getch();
+}
+
